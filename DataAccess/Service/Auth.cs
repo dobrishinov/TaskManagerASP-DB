bfa25ea48e5d3273f9b3293e74a9f49c96788625
Georgi Dobrishinov <dobrishinov@gmail.com>
2016-07-21 13:34:08 +0300

    Add AuthService

---
+++ DataAccess/Service/Auth.cs
@@ -0,0 +1,17 @@
+﻿namespace DataAccess.Service
+{
+    using System.Linq;
+    using Repository;
+    using Entity;
+
+    public class Auth
+    {
+        public UserEntity LoggedUser { get; private set; }
+
+        public void AuthenticateUser(string username, string password)
+        {
+            UsersRepository usersRepository = new UsersRepository();
+            LoggedUser = usersRepository.GetAll(u => u.Username == username && u.Password == password).FirstOrDefault();
+        }
+    }
+}
