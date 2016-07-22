2dde44f5cca49ea5c1541e276e2c64c14fc21f5e
Georgi Dobrishinov <dobrishinov@gmail.com>
2016-07-22 17:36:45 +0300

    Add TaskManager, Add Task, delete Task work

---
+++ WebTaskManagerEfDb/Controllers/TasksManagerController.cs
@@ -0,0 +1,62 @@
+﻿namespace WebTaskManager.Controllers
+{
+    using DataAccess.Repository;
+    using System.Web.Mvc;
+    using Models;
+    using DataAccess.Entity;
+
+    public class TasksManagerController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (AuthenticationManager.LoggedUser == null)
+                return RedirectToAction("Login", "Home");
+
+            TasksRepository TasksRepository = new TasksRepository();
+            ViewData["tasks"] = TasksRepository.GetAll(t => t.CreatorId == AuthenticationManager.LoggedUser.Id || t.ResponsibleUsers == AuthenticationManager.LoggedUser.Id);
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult EditTask(int? id)
+        {
+            if (AuthenticationManager.LoggedUser == null)
+                return RedirectToAction("Login", "Home");
+
+            TasksRepository tasksRepository = new TasksRepository();
+
+            TaskEntity task = null;
+            if (id == null)
+                task = new TaskEntity();
+            else
+                task = tasksRepository.GetById(id.Value);
+
+            ViewData["task"] = task;
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EditTask(TaskEntity task)
+        {
+            if (AuthenticationManager.LoggedUser == null)
+                return RedirectToAction("Login", "Home");
+
+            TasksRepository tasksRepository = new TasksRepository();
+            tasksRepository.Save(task);
+
+            return RedirectToAction("Index", "TasksManager");
+        }
+
+        public ActionResult DeleteTask(int id)
+        {
+            if (AuthenticationManager.LoggedUser == null)
+                return RedirectToAction("Login", "Home");
+
+            TasksRepository tasksRepository = new TasksRepository();
+            TaskEntity user = tasksRepository.GetById(id);
+            tasksRepository.Delete(user);
+
+            return RedirectToAction("Index", "TasksManager");
+        }
+    }
+}
---
+++ WebTaskManagerEfDb/Controllers/UserManagerController.cs
@@ -0,0 +1,62 @@
+﻿namespace WebTakManager.Controllers
+{
+    using DataAccess.Entity;
+    using DataAccess.Repository;
+    using System.Web.Mvc;
+    using WebTaskManager.Models;
+
+    public class UsersManagerController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (AuthenticationManager.LoggedUser == null)
+                return RedirectToAction("Login", "Home");
+
+            UsersRepository usersRepository = new UsersRepository();
+            ViewData["users"] = usersRepository.GetAll();
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult EditUser(int? id)
+        {
+            if (AuthenticationManager.LoggedUser == null)
+                return RedirectToAction("Login", "Home");
+
+            UsersRepository usersRepository = new UsersRepository();
+
+            UserEntity user = null;
+            if (id == null)
+                user = new UserEntity();
+            else
+                user = usersRepository.GetById(id.Value);
+
+            ViewData["user"] = user;
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EditUser(UserEntity user)
+        {
+            if (AuthenticationManager.LoggedUser == null)
+                return RedirectToAction("Login", "Home");
+
+            UsersRepository usersRepository = new UsersRepository();
+            usersRepository.Save(user);
+
+            return RedirectToAction("Index", "UsersManager");
+        }
+
+        public ActionResult DeleteUser(int id)
+        {
+            if (AuthenticationManager.LoggedUser == null)
+                return RedirectToAction("Login", "Home");
+
+            UsersRepository usersRepository = new UsersRepository();
+            UserEntity user = usersRepository.GetById(id);
+            usersRepository.Delete(user);
+
+            return RedirectToAction("Index", "UsersManager");
+        }
+    }
+}
